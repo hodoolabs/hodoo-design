b42a182d222bd6e73b027f577757298d46861e4c
TJ <tj202304@gmail.com>
2023-09-12 10:34:12 +0900

    fix: 레디오 dist

--- dist/lib/Radio/Radio.js
+++ dist/lib/Radio/Radio.js
@@ -6,7 +6,7 @@ import { HelperStyle, LabelStyle, RadioStyle } from './style';
 import RadioDisabledSvg from './RadioDisabledSvg';
 import RadioSvg from './RadioSvg';
 const Radio = ({ id, selected, label, helper, disabled = false, onChange }) => {
-    return (_jsxs("div", { className: 'inline-flex gap-3', children: [_jsx("button", { className: cn(RadioStyle({ selected: id === selected, disabled })), onClick: () => !disabled && onChange && onChange(id), children: id === selected && disabled ? _jsx(RadioDisabledSvg, {}) : _jsx(RadioSvg, {}) }), label && (_jsxs("div", { className: 'space-y-1 font-medium', children: [_jsx("label", { className: cn(LabelStyle({ disabled })), onClick: () => !disabled && onChange && onChange(id), children: label }), helper && _jsx("p", { className: cn(HelperStyle({ disabled })), children: helper })] }))] }));
+    return (_jsxs("div", { className: 'inline-flex gap-3', children: [_jsx("button", { className: cn(RadioStyle({ selected: id === selected, disabled })), onClick: () => !disabled && onChange && onChange(id), children: id === selected && (disabled ? _jsx(RadioDisabledSvg, {}) : _jsx(RadioSvg, {})) }), label && (_jsxs("div", { className: 'space-y-1 font-medium', children: [_jsx("label", { className: cn(LabelStyle({ disabled })), onClick: () => !disabled && onChange && onChange(id), children: label }), helper && _jsx("p", { className: cn(HelperStyle({ disabled })), children: helper })] }))] }));
 };
 export default memo(Radio, (prev, next) => prev.id === next.id &&
     prev.selected === next.selected &&
