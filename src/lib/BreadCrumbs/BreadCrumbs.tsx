e8149419ff6f9586ed847a632c30ebd5336b41e7
sohuichoi <sohui.choi@hodoolabs.com>
2023-09-14 15:33:40 +0900

    fix: breadcrumbs onPush 추가

--- src/lib/BreadCrumbs/BreadCrumbs.tsx
+++ src/lib/BreadCrumbs/BreadCrumbs.tsx
@@ -6,20 +6,21 @@ import { BreadCrumbsType } from '../../types/breadCrumbs';
 
 interface BreadCrumbsProps {
 	data: BreadCrumbsType;
+	onPush: (pathname: string) => void;
 	pathname: string;
 }
 
-const BreadCrumbs = ({ data, pathname }: BreadCrumbsProps) => {
+const BreadCrumbs = ({ data, onPush, pathname }: BreadCrumbsProps) => {
 	return (
 		<div className='flex gap-2 mb-3 text-sm font-medium text-gray-500'>
 			{data[pathname].bread_crumbs.map((item) => (
 				<div key={item.url} className='flex items-center gap-2 leading-5'>
-					<a
-						href={item.url}
+					<span
+						onClick={() => onPush(item.url)}
 						className='rounded hover:text-gray-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2'
 					>
 						{item.label}
-					</a>
+					</span>
 					<ChevronRightIcon className='w-4 h-4' />
 				</div>
 			))}
