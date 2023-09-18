35c2eb7b8fe9c29448a83f863364a82395d80298
TJ <tj202304@gmail.com>
2023-09-18 10:45:06 +0900

    feature: button group 완료

--- dist/lib/ButtonGroup/ButtonGroup.js
+++ dist/lib/ButtonGroup/ButtonGroup.js
@@ -1,15 +1,11 @@
 'use client';
 import { jsx as _jsx } from "react/jsx-runtime";
-import { memo, useState } from 'react';
+import { memo } from 'react';
 import { styled } from 'styled-components';
 import { cn } from '../../utils/style';
 import { ButtonStyle } from './style';
-const ButtonGroup = ({ buttons, defaultActive = 0, className }) => {
-    const [active, setActive] = useState(defaultActive ? buttons[defaultActive].id : null);
-    return (_jsx(ButtonGroupStyled, { className: className, children: buttons.map((button, index) => (_jsx("button", { className: cn(ButtonStyle({ active: active === index })), onClick: () => {
-                button.onClick;
-                setActive(index);
-            }, children: button.label }))) }));
+const ButtonGroup = ({ buttons, active, className, onActive }) => {
+    return (_jsx(ButtonGroupStyled, { className: className, children: buttons.map((button) => (_jsx("button", { className: cn(ButtonStyle({ active: active === button.id })), onClick: () => onActive(button.id), children: button.label }))) }));
 };
 export default memo(ButtonGroup, (prev, next) => prev.buttons === next.buttons && prev.className === next.className);
 const ButtonGroupStyled = styled.div `
