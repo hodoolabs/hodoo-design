1c96f49c38cfb9b3b9cdd816009c5bbc1c1b38b8
sohuichoi <sohui.choi@hodoolabs.com>
2023-10-06 12:22:54 +0900

    version: 1.3.59 tooltip ontouchStart 이벤트 추가

--- dist/lib/Tooltip/Tooltip.js
+++ dist/lib/Tooltip/Tooltip.js
@@ -37,7 +37,7 @@ const Tooltip = ({ color = 'dark', title, description, isShowArrow = true, child
             window.removeEventListener('scroll', hideTooltipOnScroll);
         };
     }, []);
-    return (_jsxs("div", { className: `relative inline-block ${className}`, onMouseEnter: () => setIsHovered(true), onMouseLeave: () => setIsHovered(false), ref: targetRef, children: [_jsx("div", { children: children }), isHovered &&
+    return (_jsxs("div", { className: `relative inline-block ${className}`, onMouseEnter: () => setIsHovered(true), onTouchStart: () => setIsHovered(true), onMouseLeave: () => setIsHovered(false), onTouchEnd: () => setIsHovered(false), ref: targetRef, children: [_jsx("div", { children: children }), isHovered &&
                 tooltipElement &&
                 ReactDOM.createPortal(_jsx("div", { className: cn(TooltipBoxStyle({ position, isShowArrow: isShowArrow ? position : null })), style: { top: `${tooltipPosition.top}px`, left: `${tooltipPosition.left}px` }, children: _jsxs("div", { className: `${cn(TooltipStyle({ color }))}`, children: [_jsx("p", { children: title }), _jsx("div", { className: cn(DescriptionStyle({ color })), children: description }), isShowArrow && (_jsx("div", { className: cn(ArrowStyle({ position })), children: color === 'white' ? _jsx(VectorWhiteSVG, {}) : _jsx(VectorDarkSvg, {}) }))] }) }), tooltipElement)] }));
 };
--- src/lib/Tooltip/Tooltip.tsx
+++ src/lib/Tooltip/Tooltip.tsx
@@ -59,7 +59,9 @@ const Tooltip = ({
 		<div
 			className={`relative inline-block ${className}`}
 			onMouseEnter={() => setIsHovered(true)}
+			onTouchStart={() => setIsHovered(true)}
 			onMouseLeave={() => setIsHovered(false)}
+			onTouchEnd={() => setIsHovered(false)}
 			ref={targetRef}
 		>
 			<div>{children}</div>
