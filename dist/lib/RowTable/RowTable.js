d165a948fb79864e666f35c9f2b423ea44453ddd
sohuichoi <sohui.choi@hodoolabs.com>
2023-10-16 18:02:35 +0900

    feat: rowtable shadow 추가 && 1.4.03

--- dist/lib/RowTable/RowTable.js
+++ dist/lib/RowTable/RowTable.js
@@ -1,11 +1,15 @@
 'use client';
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { stringify } from 'flatted';
-import { memo, useEffect, useState } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
+import { styled } from 'styled-components';
 import Tbody from './components/Tbody';
 import Thead from './components/Thead';
 const RowTable = ({ size, columns, dataSource, minWidth }) => {
+    const tableContainerRef = useRef(null);
+    const tableRef = useRef(null);
     const [sortDatas, setSortDatas] = useState(dataSource);
+    const [showShadow, setShowShadow] = useState(false);
     const handleSortDatas = (dataSource, sortDatas, sorter) => {
         const isSorted = stringify(dataSource) !== stringify(sortDatas);
         setSortDatas(isSorted
@@ -17,9 +21,24 @@ const RowTable = ({ size, columns, dataSource, minWidth }) => {
     useEffect(() => {
         setSortDatas(dataSource);
     }, [dataSource]);
-    return (_jsx("div", { className: 'overflow-x-auto', children: _jsxs("table", { className: 'w-full', style: { minWidth: `${minWidth}px` }, children: [_jsx(Thead, { size: size, columns: columns, dataSource: dataSource, sortDatas: sortDatas, onSort: handleSortDatas }), _jsx(Tbody, { size: size, columns: columns, sortDatas: sortDatas })] }) }));
+    useEffect(() => {
+        if (tableContainerRef.current && tableRef.current) {
+            if (tableContainerRef.current.offsetWidth < tableRef.current.offsetWidth) {
+                setShowShadow(true);
+            }
+        }
+    }, [tableContainerRef, tableRef]);
+    return (_jsxs("div", { className: 'relative flex overflow-x-auto', ref: tableContainerRef, children: [_jsxs("table", { className: 'w-full', style: { minWidth: `${minWidth}px` }, ref: tableRef, children: [_jsx(Thead, { size: size, columns: columns, dataSource: dataSource, sortDatas: sortDatas, onSort: handleSortDatas }), _jsx(Tbody, { size: size, columns: columns, sortDatas: sortDatas })] }), showShadow && _jsx(Shadow, {})] }));
 };
 export default memo(RowTable, (prev, next) => prev.size === next.size &&
     prev.columns === next.columns &&
     prev.dataSource === next.dataSource &&
     prev.minWidth === next.minWidth);
+const Shadow = styled.div `
+	position: sticky;
+	content: '';
+	right: 0;
+	top: 0;
+	bottom: 0;
+	box-shadow: 0 0 60px 30px #fff, 0 0 100px 60px rgba(255, 255, 255, 0);
+`;
--- src/lib/RowTable/RowTable.tsx
+++ src/lib/RowTable/RowTable.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { stringify } from 'flatted';
-import { memo, useEffect, useState } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
+import { styled } from 'styled-components';
+import { ColumnDataType, ColumnType } from '../../types/table';
 import Tbody from './components/Tbody';
 import Thead from './components/Thead';
-import { ColumnDataType, ColumnType } from '../../types/table';
 
 interface RowTableProps {
 	size: 'lg' | 'sm';
@@ -14,7 +15,10 @@ interface RowTableProps {
 }
 
 const RowTable = ({ size, columns, dataSource, minWidth }: RowTableProps) => {
+	const tableContainerRef = useRef<HTMLDivElement>(null);
+	const tableRef = useRef<HTMLTableElement>(null);
 	const [sortDatas, setSortDatas] = useState(dataSource);
+	const [showShadow, setShowShadow] = useState(false);
 
 	const handleSortDatas = (
 		dataSource: ColumnDataType[],
@@ -36,12 +40,21 @@ const RowTable = ({ size, columns, dataSource, minWidth }: RowTableProps) => {
 		setSortDatas(dataSource);
 	}, [dataSource]);
 
+	useEffect(() => {
+		if (tableContainerRef.current && tableRef.current) {
+			if (tableContainerRef.current.offsetWidth < tableRef.current.offsetWidth) {
+				setShowShadow(true);
+			}
+		}
+	}, [tableContainerRef, tableRef]);
+
 	return (
-		<div className='overflow-x-auto'>
-			<table className='w-full' style={{ minWidth: `${minWidth}px` }}>
+		<div className='relative flex overflow-x-auto' ref={tableContainerRef}>
+			<table className='w-full' style={{ minWidth: `${minWidth}px` }} ref={tableRef}>
 				<Thead size={size} columns={columns} dataSource={dataSource} sortDatas={sortDatas} onSort={handleSortDatas} />
 				<Tbody size={size} columns={columns} sortDatas={sortDatas} />
 			</table>
+			{showShadow && <Shadow />}
 		</div>
 	);
 };
@@ -54,3 +67,12 @@ export default memo(
 		prev.dataSource === next.dataSource &&
 		prev.minWidth === next.minWidth
 );
+
+const Shadow = styled.div`
+	position: sticky;
+	content: '';
+	right: 0;
+	top: 0;
+	bottom: 0;
+	box-shadow: 0 0 60px 30px #fff, 0 0 100px 60px rgba(255, 255, 255, 0);
+`;
