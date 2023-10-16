9f7f234e7567df5dfdb872fc0eff8f7051d315dd
HodooSeungMo <seungmo.kim@hodoolabs.com>
2023-10-16 12:50:53 +0900

    feat: modal 원복, 버젼업

--- src/lib/Modal/style.ts
+++ src/lib/Modal/style.ts
@@ -1,5 +1,14 @@
 import { cva } from 'class-variance-authority';
 
+const ModalContainerStyle = cva(['fixed top-0 left-0 z-30 flex justify-center w-full h-screen'], {
+	variants: {
+		isMobile: {
+			true: ['bottom-0', 'items-end'],
+			false: ['top-0', 'items-center'],
+		},
+	},
+});
+
 const ModalStyle = cva(['w-full', 'shadow', 'z-50'], {
 	variants: {
 		size: {
@@ -9,4 +18,12 @@ const ModalStyle = cva(['w-full', 'shadow', 'z-50'], {
 	},
 });
 
-export { ModalStyle };
+const ModalContentStyle = cva(['px-6 pb-6 bg-white'], {
+	variants: {
+		isMobile: {
+			false: ['rounded-b-3xl'],
+		},
+	},
+});
+
+export { ModalStyle, ModalContainerStyle, ModalContentStyle };
