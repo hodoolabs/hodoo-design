67008e788e36a39b993d8098ac523accf32a5ea1
HodooSeungMo <seungmo.kim@hodoolabs.com>
2023-10-16 12:53:29 +0900

    feat: 누락된 부분 추가, 버젼 업

--- dist/lib/Modal/Modal.d.ts
+++ dist/lib/Modal/Modal.d.ts
@@ -3,7 +3,8 @@ export interface ModalProps {
     modalState: ModalStateType;
     modalHistory?: ModalStateType[];
     goBackModal?: () => void;
+    isMobile?: boolean;
     closeModal: () => void;
 }
-declare const Modal: ({ modalState, modalHistory, goBackModal, closeModal }: ModalProps) => import("react/jsx-runtime").JSX.Element;
+declare const Modal: ({ modalState, modalHistory, goBackModal, closeModal, isMobile }: ModalProps) => import("react/jsx-runtime").JSX.Element;
 export default Modal;
