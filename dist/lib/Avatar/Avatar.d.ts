8837822086c2935f60b73708fc44eecdb508e79c
TJ <tj202304@gmail.com>
2023-10-19 17:08:08 +0900

    feaure: avatar

--- dist/lib/Avatar/Avatar.d.ts
+++ dist/lib/Avatar/Avatar.d.ts
@@ -2,6 +2,7 @@
 interface AvatarProps {
     size: 'sm' | 'regular' | 'md' | 'lg' | 'xl';
     img: string;
+    className?: string;
 }
-declare const _default: import("react").MemoExoticComponent<({ size, img }: AvatarProps) => import("react/jsx-runtime").JSX.Element>;
+declare const _default: import("react").MemoExoticComponent<({ size, img, className }: AvatarProps) => import("react/jsx-runtime").JSX.Element>;
 export default _default;
--- src/lib/Avatar/Avatar.tsx
+++ src/lib/Avatar/Avatar.tsx
@@ -7,10 +7,11 @@ import { AvatarStyle } from './style';
 interface AvatarProps {
 	size: 'sm' | 'regular' | 'md' | 'lg' | 'xl';
 	img: string;
+	className?: string;
 }
 
-const Avatar = ({ size, img }: AvatarProps) => {
-	return <img src={img} alt='avatar' className={cn(AvatarStyle({ size }))} />;
+const Avatar = ({ size, img, className }: AvatarProps) => {
+	return <img src={img} alt='avatar' className={`${cn(AvatarStyle({ size }))} ${className}`} />;
 };
 
 export default memo(Avatar, (prev: AvatarProps, next: AvatarProps) => prev.size === next.size && prev.img === next.img);
