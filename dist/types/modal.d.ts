5192570596f73fd1cc2bf1f6b7a869e3aefb947e
HodooSeungMo <seungmo.kim@hodoolabs.com>
2023-11-07 12:41:11 +0900

    chore: 버젼 업

--- dist/types/modal.d.ts
+++ dist/types/modal.d.ts
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 type ModalType = {
     modalState: ModalStateType;
     modalHistory: ModalStateType[];
-    openModal: ({ title, subTitle, content, size, position }: ModalStateType) => void;
+    openModal: ({ title, subTitle, content, size }: ModalStateType) => void;
     goBackModal: () => void;
     closeModal: () => void;
 };
@@ -10,8 +10,7 @@ type ModalStateType = {
     title: string;
     subTitle?: ReactNode;
     content: ReactNode;
-    size?: 'lg' | 'sm';
-    position?: 'middle' | 'bottom';
+    size?: 'lg' | 'sm' | 'mobile';
     isOpen?: boolean;
 };
 export type { ModalStateType, ModalType };
