e9aa8812fa42347cc75683f2bf4fba99ae87c4a0
TJ <tj202304@gmail.com>
2023-11-14 15:52:57 +0900

    feature: 스피너 size 변경

--- dist/lib/Spinner/Spinner.js
+++ dist/lib/Spinner/Spinner.js
@@ -2,7 +2,7 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import styled from 'styled-components';
 import SpinnerSvg from './images/SpinnerSvg';
-const Spinner = ({ isLoading, size = 'medium' }) => {
+const Spinner = ({ isLoading, size = 'md' }) => {
     return (_jsxs(SpinnerStyled, { className: `fixed top-0 left-0 w-full flex items-center justify-center h-full z-[9999999999] opacity-0 pointer-events-none ${isLoading && 'spin-fade-in'}`, children: [_jsx("div", { className: 'absolute top-0 left-0 w-full h-full bg-white opacity-70' }), _jsx(SpinnerSvg, { size: size })] }));
 };
 export default Spinner;
