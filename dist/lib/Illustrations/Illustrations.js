5481879f1e6b338bc004c8834687c98ed7da0da8
TJ <tj202304@gmail.com>
2023-12-05 10:59:14 +0900

    feature: version: 1.5.07

--- dist/lib/Illustrations/Illustrations.js
+++ dist/lib/Illustrations/Illustrations.js
@@ -2,6 +2,6 @@ import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import ListSvg from './images/List';
 import NoResultsSvg from './images/NoResultsSvg';
 const Illustrations = ({ type = 'noReseults', description }) => {
-    return (_jsxs("div", { className: 'flex flex-col gap-4 items-center', children: [type === 'list' && _jsx(ListSvg, {}), type === 'noReseults' && _jsx(NoResultsSvg, {}), _jsx("div", { className: 'text-center', children: description })] }));
+    return (_jsxs("div", { className: 'flex flex-col gap-4 items-center', children: [type === 'list' && _jsx(ListSvg, {}), type === 'noReseults' && _jsx(NoResultsSvg, {}), _jsx("div", { className: 'text-center text-slate-500 text-base font-medium', children: description })] }));
 };
 export default Illustrations;
