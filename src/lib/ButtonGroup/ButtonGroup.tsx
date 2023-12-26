7424a8955898917c675f290e43291002c2b844b5
rhesus2016 <rhesus2016@gmail.com>
2023-12-26 20:31:57 +0900

    featyre: buttonGroup Type 추가

--- src/lib/ButtonGroup/ButtonGroup.tsx
+++ src/lib/ButtonGroup/ButtonGroup.tsx
@@ -1,28 +1,72 @@
 'use client';
 
+import { ReactNode } from 'react';
 import { ButtonGroupType } from '../../types/buttonGroup';
 import { cn } from '../../utils/style';
+import Button from '../Button/Button';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import Helper from '../Helper/Helper';
+import Label from '../Label/Label';
 import { ButtonStyle } from './style';
 
 interface ButtonGroupProps {
-	buttons: ButtonGroupType[];
+	type: 'primary' | 'secondary';
 	active: string;
+	error?: string;
+	buttons: ButtonGroupType[];
+	label?: ReactNode;
+	helper?: ReactNode;
+	disabled?: boolean;
+	required?: boolean;
 	className?: string;
-	onActive: (value: string) => void;
+	onActive: (active: string) => void;
+	onError?: (error: string) => void;
 }
 
-const ButtonGroup = ({ buttons, active, className, onActive }: ButtonGroupProps) => {
+const ButtonGroup = ({
+	type = 'primary',
+	active,
+	error,
+	buttons,
+	label,
+	helper,
+	disabled,
+	required,
+	className,
+	onActive,
+	onError,
+}: ButtonGroupProps) => {
 	return (
-		<div className={className}>
-			{buttons.map((button) => (
-				<button
-					key={button.value}
-					className={cn(ButtonStyle({ active: active === button.value }))}
-					onClick={() => onActive(button.value)}
-				>
-					{button.label}
-				</button>
-			))}
+		<div className={`flex flex-col ${className}`}>
+			<Label error={error} label={label} disabled={disabled} required={required} />
+			{type === 'primary' ? (
+				<div>
+					{buttons.map((button) => (
+						<button
+							key={button.value}
+							className={cn(ButtonStyle({ active: active === button.value }))}
+							onClick={() => onActive(button.value)}
+						>
+							{button.label}
+						</button>
+					))}
+				</div>
+			) : type === 'secondary' ? (
+				<div className='flex gap-2'>
+					{buttons.map((item) => (
+						<Button
+							key={item.value}
+							color={active !== item.value ? 'white_line' : 'blue'}
+							size='md'
+							label={item.label}
+							disabled={active !== item.value && disabled}
+							onClick={() => onActive(item.value)}
+						/>
+					))}
+				</div>
+			) : null}
+			<Helper size='lg' error={error} helper={helper} disabled={disabled} />
+			<ErrorMessage size='lg' error={error} />
 		</div>
 	);
 };
