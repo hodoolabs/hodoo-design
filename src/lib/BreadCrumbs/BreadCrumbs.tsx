4005b088aa14a2842ab64aa4187040209a104694
taejin-k <tj202304@gmail.com>
2024-02-14 10:39:04 +0900

    refactor: Badge, BreadCrumbs 검수 완료

--- src/lib/BreadCrumbs/BreadCrumbs.tsx
+++ src/lib/BreadCrumbs/BreadCrumbs.tsx
@@ -11,18 +11,24 @@ interface BreadCrumbsProps {
 }
 
 const BreadCrumbs = ({ data, path, className, onPush }: BreadCrumbsProps) => {
+	const breadCrumbs = data[path]?.bread_crumbs;
+
 	return (
-		<div className={`flex gap-2 text-sm font-medium text-gray-500 ${className}`}>
-			{data[path].bread_crumbs?.map((item) => (
-				<div key={item.path} className='flex items-center gap-2'>
-					<span onClick={() => onPush(item.path)} className='rounded hover:text-gray-700 cursor-pointer'>
-						{item.label}
-					</span>
-					<ChevronRightIcon className='w-4 h-4' />
+		<>
+			{!!breadCrumbs?.length && (
+				<div className={`flex gap-2 text-sm font-medium text-gray-500 ${className}`}>
+					{breadCrumbs?.map((item) => (
+						<div key={item.path} className='flex items-center gap-2'>
+							<span onClick={() => onPush(item.path)} className='rounded cursor-pointer hover:text-gray-700'>
+								{item.label}
+							</span>
+							<ChevronRightIcon className='w-4 h-4' />
+						</div>
+					))}
+					<span className='text-gray-400'>{data[path].label}</span>
 				</div>
-			))}
-			<span className='text-gray-400'>{data[path].label}</span>
-		</div>
+			)}
+		</>
 	);
 };
 
