c594837c1e0076827cc447d474ff541c11d394e2
taejin-k <tj202304@gmail.com>
2024-02-15 12:32:02 +0900

    version:1.6.29

--- dist/lib/Illustrations/Illustrations.d.ts
+++ dist/lib/Illustrations/Illustrations.d.ts
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 interface IllustrationsProps {
-    type?: 'list' | 'noReseults';
-    description: ReactNode;
+    type?: 'list' | 'noResults';
+    description?: ReactNode;
     className?: string;
 }
 declare const Illustrations: ({ type, description, className }: IllustrationsProps) => import("react/jsx-runtime").JSX.Element;
--- dist/lib/Illustrations/Illustrations.js
+++ dist/lib/Illustrations/Illustrations.js
@@ -1,7 +1,7 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import ListSvg from './images/List';
+import ListSvg from './images/ListSvg';
 import NoResultsSvg from './images/NoResultsSvg';
-const Illustrations = ({ type = 'noReseults', description, className }) => {
-    return (_jsx("div", { className: `flex items-center justify-center w-full ${className}`, children: _jsxs("div", { className: 'flex flex-col gap-4 items-center', children: [type === 'list' && _jsx(ListSvg, {}), type === 'noReseults' && _jsx(NoResultsSvg, {}), _jsx("div", { className: 'text-center text-gray-500 text-base font-medium whitespace-pre-line', children: description })] }) }));
+const Illustrations = ({ type = 'noResults', description, className }) => {
+    return (_jsx("div", { className: `flex items-center justify-center w-full ${className}`, children: _jsxs("div", { className: 'flex flex-col items-center gap-4', children: [type === 'list' && _jsx(ListSvg, {}), type === 'noResults' && _jsx(NoResultsSvg, {}), _jsx("div", { className: 'text-base font-medium text-center text-gray-500 whitespace-pre-line', children: description })] }) }));
 };
 export default Illustrations;
