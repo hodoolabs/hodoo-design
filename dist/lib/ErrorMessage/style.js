6c22e286fc9542db8eea59bf00de38749a52e0c2
sohuichoi <sohui.choi@hodoolabs.com>
2024-07-05 09:33:08 +0900

    style: error message min height 설정

--- dist/lib/ErrorMessage/style.js
+++ dist/lib/ErrorMessage/style.js
@@ -1,14 +1,14 @@
 import { cva } from 'class-variance-authority';
-const ErrorMessageStyle = cva(['font-medium overflow-hidden text-red-600 duration-300'], {
+const ErrorMessageStyle = cva(['font-medium overflow-hidden text-red-600 duration-300 transition-all'], {
     variants: {
         size: {
-            lg: ['text-sm h-7'],
-            md: ['text-xs h-6'],
-            sm: ['text-xs h-6'],
+            lg: ['text-sm min-h-7'],
+            md: ['text-xs min-h-6'],
+            sm: ['text-xs min-h-6'],
         },
         isError: {
             true: ['pt-2'],
-            false: ['h-0 pt-0'],
+            false: ['min-h-0 pt-0'],
         },
     },
 });
