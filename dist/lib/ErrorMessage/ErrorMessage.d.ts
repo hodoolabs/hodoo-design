5ab0bf6b81575d4ae1d70b3fbee2f3138535204e
hodooseungmo <seungmo.kim@hodoolabs.com>
2024-07-15 11:49:45 +0900

    fix: 에러메시지 클래스네임 프롭스 추가

--- dist/lib/ErrorMessage/ErrorMessage.d.ts
+++ dist/lib/ErrorMessage/ErrorMessage.d.ts
@@ -2,6 +2,7 @@ interface ErrorMessageProps {
     size?: any;
     error?: string;
     errorRow?: 1 | 2;
+    className?: string;
 }
-declare const ErrorMessage: ({ size, error, errorRow }: ErrorMessageProps) => import("react/jsx-runtime").JSX.Element;
+declare const ErrorMessage: ({ size, error, errorRow, className }: ErrorMessageProps) => import("react/jsx-runtime").JSX.Element;
 export default ErrorMessage;
