f2912c2474c84e812e2724474bb6a0f890aefd32
endmoseung <endmoseung>
2024-09-24 17:41:19 +0900

    feat: input에 ref 추가 - ref forwardRef로 감싼 리뷰 적용

--- dist/lib/Input/Input.js
+++ dist/lib/Input/Input.js
@@ -1,12 +1,12 @@
 'use client';
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { useEffect } from 'react';
+import { forwardRef, useEffect } from 'react';
 import { cn } from '../../utils/style';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import Helper from '../Helper/Helper';
 import Label from '../Label/Label';
 import { InputStyle } from './style';
-const Input = ({ type = 'text', size = 'lg', autoComplete = 'off', label, value, error, maxLength, placeholder, helper, disabled, required, className, onBlur, onChange, onError, onEnter, }) => {
+const Input = forwardRef(({ type = 'text', size = 'lg', autoComplete = 'off', label, value, error, maxLength, placeholder, helper, disabled, required, className, onBlur, onChange, onError, onEnter, }, ref) => {
     useEffect(() => {
         if (!onError)
             return;
@@ -20,6 +20,6 @@ const Input = ({ type = 'text', size = 'lg', autoComplete = 'off', label, value,
             return;
         onChange(value);
     };
-    return (_jsxs("div", { className: `flex flex-col ${className}`, children: [_jsx(Label, { size: size, value: value, error: error, label: label, maxLength: maxLength, disabled: disabled, required: required }), _jsx("input", { type: type, value: value, maxLength: maxLength, placeholder: placeholder, disabled: disabled, autoComplete: autoComplete, className: cn(InputStyle({ size, error: !!error })), onBlur: (event) => onBlur && onBlur(event.target.value), onChange: handleChange, onKeyDown: (event) => event.key === 'Enter' && onEnter && onEnter() }), _jsx(Helper, { size: size, error: error, helper: helper, disabled: disabled }), _jsx(ErrorMessage, { size: size, error: error })] }));
-};
+    return (_jsxs("div", { className: `flex flex-col ${className}`, children: [_jsx(Label, { size: size, value: value, error: error, label: label, maxLength: maxLength, disabled: disabled, required: required }), _jsx("input", { type: type, value: value, maxLength: maxLength, placeholder: placeholder, disabled: disabled, autoComplete: autoComplete, className: cn(InputStyle({ size, error: !!error })), onBlur: (event) => onBlur && onBlur(event.target.value), onChange: handleChange, onKeyDown: (event) => event.key === 'Enter' && onEnter && onEnter(), ref: ref }), _jsx(Helper, { size: size, error: error, helper: helper, disabled: disabled }), _jsx(ErrorMessage, { size: size, error: error })] }));
+});
 export default Input;
