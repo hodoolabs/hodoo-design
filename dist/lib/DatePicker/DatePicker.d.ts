a3e250aa710d0d3cc629f8a243875427ef1a5326
hodooSeungmo <seungmo.kim@hodoolabs.com>
2024-12-12 10:34:20 +0900

    fix: 꼬인 의존성 수정

--- dist/lib/DatePicker/DatePicker.d.ts
+++ dist/lib/DatePicker/DatePicker.d.ts
@@ -3,7 +3,6 @@ import { DatepickerType } from 'react-tailwindcss-datepicker';
 import DefaultI18nAdapter from './defaultAdapter';
 import NextIntlAdapter from './next-intl-Adapter';
 import { I18nAdapter } from './types';
-export declare const configureDatePickerI18n: (adapter: I18nAdapter) => void;
 interface DatePickerProps extends DatepickerType {
     size?: 'lg' | 'sm';
     label?: ReactNode;
@@ -16,4 +15,12 @@ interface DatePickerProps extends DatepickerType {
 }
 declare const DatePicker: ({ size, label, error, helper, required, disabled, className, onError, onChange, ...props }: DatePickerProps) => import("react/jsx-runtime").JSX.Element;
 export default DatePicker;
-export { DefaultI18nAdapter, NextIntlAdapter };
+declare const configureDatePickerI18n: (adapter: I18nAdapter) => void;
+/**
+ * @description 만약 next-intl을 사용하는 경우 앱 최초로 init 하는곳에서 nextintladapter로 configureDatePickerI18n를 실행
+ * @example
+ * // 앱 최상단 init 하는곳
+ * import NextIntlAdapter from './next-intl-Adapter';
+ * configureDatePickerI18n(new NextIntlAdapter());
+ */
+export { DefaultI18nAdapter, NextIntlAdapter, configureDatePickerI18n };
--- dist/lib/DatePicker/DatePicker.js
+++ dist/lib/DatePicker/DatePicker.js
@@ -23,10 +23,6 @@ import { InputStyle, SkeletonStyle, ToggleStyle } from './style';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import DefaultI18nAdapter from './defaultAdapter';
 import NextIntlAdapter from './next-intl-Adapter';
-let i18nAdapter = new DefaultI18nAdapter();
-export const configureDatePickerI18n = (adapter) => {
-    i18nAdapter = adapter;
-};
 const DatePicker = (_a) => {
     var _b, _c;
     var { size = 'lg', label, error, helper, required, disabled = false, className, onError, onChange } = _a, props = __rest(_a, ["size", "label", "error", "helper", "required", "disabled", "className", "onError", "onChange"]);
@@ -58,4 +54,15 @@ const DatePicker = (_a) => {
     return (_jsxs("div", { className: `flex flex-col ${className}`, children: [_jsx(Label, { size: size, error: error, label: label, disabled: disabled, required: required }), !isDestory ? (_jsx(Datepicker, Object.assign({ i18n: locale, readOnly: true, displayFormat: props.displayFormat, startFrom: ((_b = props.value) === null || _b === void 0 ? void 0 : _b.startDate) ? new Date((_c = props.value) === null || _c === void 0 ? void 0 : _c.startDate) : new Date(), toggleIcon: () => _jsx(CalendarIcon, { className: 'w-5 h-5' }), toggleClassName: cn(ToggleStyle({ size, error: !!error, disabled })), inputClassName: cn(InputStyle({ size, error: !!error, disabled })), placeholder: props.placeholder || ' ', disabled: disabled, onChange: (date, event) => event && handleChangeDate(date, event, props.placeholder) }, props))) : (_jsx("div", { className: cn(SkeletonStyle({ size })) })), _jsx(Helper, { size: size, error: error, helper: helper, disabled: disabled }), _jsx(ErrorMessage, { size: size, error: error })] }));
 };
 export default DatePicker;
-export { DefaultI18nAdapter, NextIntlAdapter };
+let i18nAdapter = new DefaultI18nAdapter();
+const configureDatePickerI18n = (adapter) => {
+    i18nAdapter = adapter;
+};
+/**
+ * @description 만약 next-intl을 사용하는 경우 앱 최초로 init 하는곳에서 nextintladapter로 configureDatePickerI18n를 실행
+ * @example
+ * // 앱 최상단 init 하는곳
+ * import NextIntlAdapter from './next-intl-Adapter';
+ * configureDatePickerI18n(new NextIntlAdapter());
+ */
+export { DefaultI18nAdapter, NextIntlAdapter, configureDatePickerI18n };
