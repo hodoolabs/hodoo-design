66983a66b4e912214491ffdd652d6447986b76e2
hodooSeungmo <seungmo.kim@hodoolabs.com>
2025-03-24 12:14:42 +0900

    fix: 버튼 disabled일때 hover기능 삭제

--- dist/lib/Button/style.d.ts
+++ dist/lib/Button/style.d.ts
@@ -2,6 +2,7 @@ declare const ButtonStyle: (props?: ({
     color?: "black" | "blue" | "gray" | "red" | "white" | "white_line" | null | undefined;
     size?: "lg" | "md" | "sm" | "base" | null | undefined;
     iconOnly?: "lg" | "md" | "sm" | "base" | null | undefined;
+    disabled?: boolean | null | undefined;
 } & import("class-variance-authority/dist/types").ClassProp) | undefined) => string;
 declare const ImageStyle: (props?: ({
     size?: "lg" | "md" | "sm" | "base" | null | undefined;
