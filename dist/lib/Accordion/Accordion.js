7dfe8199960af9bd8067e4d43f8362513c0b54d1
hodooSeungmo <seungmo.kim@hodoolabs.com>
2025-04-04 15:55:44 +0900

    fix: 계산 오류 + 메뉴 커스텀 색깔 적용되도록

--- dist/lib/Accordion/Accordion.js
+++ dist/lib/Accordion/Accordion.js
@@ -34,12 +34,13 @@ const Accordion = ({ list, path, className, menuItem, onPush }) => {
             setExpandedMenuIndex(index);
     };
     return (_jsx("div", { className: `text-base font-semibold flex flex-col gap-3 ${className}`, children: list.map((item) => {
-            var _a, _b;
+            var _a, _b, _c;
             const isExpanded = getIsExpandedMenu(item.index, expandedMenuIndex);
-            return (_jsxs("div", { children: [_jsxs("div", { style: getMenuItemStyle(getIsCurrentPath(item.menu.path, path)), className: cn(MenuStyle({ isCurrentPath: getIsCurrentPath(item.menu.path, path) }), getMenuItemClass()), onClick: () => handleMenuClick(item.index, expandedMenuIndex, item.menu.path, item.subMenus), children: [_jsx("img", { src: item[getIsCurrentPath(item.menu.path, path) ? 'activeIcon' : 'icon'], alt: item.icon, className: 'w-6 h-6' }), _jsx("span", { children: item.menu.label }), !!item.subMenus && _jsx(ChevronDownIcon, { className: cn(ArrowStyle({ isExpanded })) })] }), !!item.subMenus && (_jsx("div", { className: 'flex flex-col gap-1 overflow-hidden transition-all duration-300 ease-in-out', style: {
-                            height: isExpanded ? `${((_a = item.subMenus) === null || _a === void 0 ? void 0 : _a.length) * 48}px` : '0px',
+            const isCurrentPath = getIsCurrentPath(item.menu.path, path);
+            return (_jsxs("div", { children: [_jsxs("div", { style: getMenuItemStyle(isCurrentPath), className: cn(MenuStyle({ isCurrentPath }), getMenuItemClass()), onClick: () => handleMenuClick(item.index, expandedMenuIndex, item.menu.path, item.subMenus), children: [_jsx("img", { src: item[getIsCurrentPath(item.menu.path, path) ? 'activeIcon' : 'icon'], alt: item.icon, className: 'w-6 h-6' }), _jsx("span", { children: item.menu.label }), !!item.subMenus && _jsx(ChevronDownIcon, { className: cn(ArrowStyle({ isExpanded })) })] }), !!item.subMenus && (_jsx("div", { className: 'flex flex-col gap-1 overflow-hidden transition-all duration-300 ease-in-out', style: {
+                            height: isExpanded ? `${((_a = item.subMenus) === null || _a === void 0 ? void 0 : _a.length) * 48 + (((_b = item.subMenus) === null || _b === void 0 ? void 0 : _b.length) - 1) * 4}px` : '0px',
                             opacity: isExpanded ? 1 : 0,
-                        }, children: (_b = item.subMenus) === null || _b === void 0 ? void 0 : _b.map((subItem, index) => (_jsx("div", { className: cn(SubMenuStyle({ isCurrentPath: getIsCurrentPath(subItem.path, path) })), onClick: () => onPush(subItem.path), children: subItem.label }, index))) }))] }, item.index));
+                        }, children: (_c = item.subMenus) === null || _c === void 0 ? void 0 : _c.map((subItem, index) => (_jsx("div", { style: getMenuItemStyle(getIsCurrentPath(subItem.path, path)), className: cn(SubMenuStyle({ isCurrentPath: getIsCurrentPath(subItem.path, path) })), onClick: () => onPush(subItem.path), children: subItem.label }, index))) }))] }, item.index));
         }) }));
 };
 export default Accordion;
